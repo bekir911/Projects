cfd3f5e57fd2d58a81cca0c523c8509f25cbd77d
Bekir <53034558+bekir911@users.noreply.github.com>
2021-05-04 17:30:02 +0300

    Update Fixture.py

--- Fixture/Fixture.py
+++ Fixture/Fixture.py
@@ -10,7 +10,7 @@ for i in range(1, 18):
     print("\n", (2 * i - 1),". Hafta", sep = "")
 
     for j in range(1, 10):
-        print(a[j - 1], a[-j], sep = "-")
+        print(a[j - 1], a[-j], sep = " - ")
 
     a.insert(1, a[-1]), a.pop()
 
@@ -22,7 +22,7 @@ for i in range(1, 18):
     print("\n", 2 * i, ". Hafta", sep = "")
 
     for j in range(1, 10):
-        print(a[-j], a[j-1], sep = "-")
+        print(a[-j], a[j-1], sep = " - ")
 
     a.insert(1, a[-1]), a.pop()
 
